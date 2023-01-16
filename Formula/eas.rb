84c26fb44b052f62a6e91b0cd859f95ecf083237
5ouma <101255979+5ouma@users.noreply.github.com>
2023-01-16 20:30:42 +0900

    [add] `macos_keyboard_shortcuts_exporter_importer`
    Add it as `eas` to save `App Shortcuts`.

---
+++ Formula/eas.rb
@@ -0,0 +1,13 @@
+class Eas < Formula
+  version "1.1"
+  sha256 "e6398ce7b989dd0e70798b7285104032e1992dabd121ac280cf27d46687dae8f"
+
+  url "https://gist.githubusercontent.com/miclf/bf4b0cb6de9ead726197db7ed3d937b5/raw/a135140b52014273d59567f24983ded99e30ac2d/macos_keyboard_shortcuts_exporter_importer.php"
+  desc "A small tool to automatically export and import custom macOS keyboard shortcuts."
+  homepage "https://gist.github.com/miclf/bf4b0cb6de9ead726197db7ed3d937b5"
+  head "https://gist.github.com/bf4b0cb6de9ead726197db7ed3d937b5.git"
+
+  def install
+    bin.install "macos_keyboard_shortcuts_exporter_importer.php" =>  "eas"
+  end
+end
