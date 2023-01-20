4c4279e7aab48e628b9a8073f1ebd52e22607a2a
5ouma <101255979+5ouma@users.noreply.github.com>
2023-01-20 17:15:33 +0900

    [upgrade] Version up memo to 0.2.2
    Change grammar as recommended.

--- Formula/memo.rb
+++ Formula/memo.rb
@@ -1,6 +1,6 @@
 class Memo < Formula
-  version "0.2.1"
-  sha256 "338602486e399b9ddfc03233a28a302c04d9a9839081ac095ec9a29460113042"
+  version "0.2.2"
+  sha256 "37463e2558cc90dab401f6007f2f2d55394626d63bee370e09c09b53db5fa1b2"
 
   url "https://gitfront.io/r/Souma/t3rZXcDgwk5A/memo/raw/bin/memo"
   desc "This command tell you some useful commands."
