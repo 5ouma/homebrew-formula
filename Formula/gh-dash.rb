c9c73ca23c1155230f713cdc2253683f1fdf0682
5ouma <101255979+5ouma@users.noreply.github.com>
2023-12-10 19:12:54 +0900

    fix(ruby): Make it possible to run `brew bump-formula-pr`
    Because hard-coded versions can only available to it.

--- Formula/gh-dash.rb
+++ Formula/gh-dash.rb
@@ -6,14 +6,14 @@ class GhDash < Formula
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/dlvhdr/gh-dash/releases/download/v#{version}/darwin-amd64"
+      url "https://github.com/dlvhdr/gh-dash/releases/download/v3.11.0/darwin-amd64"
       sha256 "0e64c760cbada7320a6181d9946b398310c8d638d1be790210dd1cf2480ccb88"
       def install
         bin.install "darwin-amd64" => name.to_s
       end
     end
     if Hardware::CPU.arm?
-      url "https://github.com/dlvhdr/gh-dash/releases/download/v#{version}/darwin-arm64"
+      url "https://github.com/dlvhdr/gh-dash/releases/download/v3.11.0/darwin-arm64"
       sha256 "e8a25710238f9720dcc0092da3f2ad444b1cc98f3a4027afd5a1f73538353e4e"
       def install
         bin.install "darwin-arm64" => name.to_s
@@ -23,14 +23,14 @@ class GhDash < Formula
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/dlvhdr/gh-dash/releases/download/v#{version}/linux-amd64"
+      url "https://github.com/dlvhdr/gh-dash/releases/download/v3.11.0/linux-amd64"
       sha256 "6d07e381afcf04b0baecd47d39da0b8a073f78f476ebdadc47e5d0e3eb2965ae"
       def install
         bin.install "linux-amd64" => name.to_s
       end
     end
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/dlvhdr/gh-dash/releases/download/v#{version}/linux-arm64"
+      url "https://github.com/dlvhdr/gh-dash/releases/download/v3.11.0/linux-arm64"
       sha256 "ba6195aa2957b34f2b9e19ca7f27f6199545fff10f073874c6f2b8c40f25cf16"
       def install
         bin.install "linux-arm64" => name.to_s
--- Formula/ogpk.rb
+++ Formula/ogpk.rb
@@ -8,34 +8,34 @@ class Ogpk < Formula
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/almonk/ogpk/releases/download/#{version}/ogpk-#{version}-darwin-amd64"
+      url "https://github.com/almonk/ogpk/releases/download/0.1.3/ogpk-0.1.3-darwin-amd64"
       sha256 "e5006c7fcd6f7337cb5a77ae2ba4fb531728e9fff8f882f36139fbaed92baa63"
       def install
-        bin.install "ogpk-#{version}-darwin-amd64" => name.to_s
+        bin.install "ogpk-0.1.3-darwin-amd64" => name.to_s
       end
     end
     if Hardware::CPU.arm?
-      url "https://github.com/almonk/ogpk/releases/download/#{version}/ogpk-#{version}-darwin-arm64"
+      url "https://github.com/almonk/ogpk/releases/download/0.1.3/ogpk-0.1.3-darwin-arm64"
       sha256 "296ec2d80a2de5d711e438827e27d76ec1b5ba31690af39f38668921f9ec1a4e"
       def install
-        bin.install "ogpk-#{version}-darwin-arm64" => name.to_s
+        bin.install "ogpk-0.1.3-darwin-arm64" => name.to_s
       end
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/almonk/ogpk/releases/download/#{version}/ogpk-#{version}-linux-amd64"
+      url "https://github.com/almonk/ogpk/releases/download/0.1.3/ogpk-0.1.3-linux-amd64"
       sha256 "e821b6ba9c6cd27628caa9a50fa2c48dc81f08f2d3284128b49eaba583c2f2f3"
       def install
-        bin.install "ogpk-#{version}-linux-amd64" => name.to_s
+        bin.install "ogpk-0.1.3-linux-amd64" => name.to_s
       end
     end
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/almonk/ogpk/releases/download/#{version}/ogpk-#{version}-linux-arm64"
+      url "https://github.com/almonk/ogpk/releases/download/0.1.3/ogpk-0.1.3-linux-arm64"
       sha256 "3a203121f68edc135a7bc79756d74c6f3d0438af7c5674b75d873019cf9eadc1"
       def install
-        bin.install "ogpk-#{version}-linux-arm64" => name.to_s
+        bin.install "ogpk-0.1.3-linux-arm64" => name.to_s
       end
     end
   end
