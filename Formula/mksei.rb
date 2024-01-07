ba9b156129ebc51d8fa62b924e62eb2261db9010
5ouma <101255979+5ouma@users.noreply.github.com>
2024-01-07 11:52:22 +0900

    mksei 1.2.1

--- Formula/mksei.rb
+++ Formula/mksei.rb
@@ -1,9 +1,9 @@
 class Mksei < Formula
   desc "Small tool to automatically export and import custom macOS keyboard shortcuts"
   homepage "https://gist.github.com/5ouma/0a5717868f21a29ab96c914ddd55a409"
-  url "https://gist.githubusercontent.com/5ouma/0a5717868f21a29ab96c914ddd55a409/raw/aa46f5ac940e522a3a4b7665d30b7598eaf89750/macos_keyboard_shortcuts_exporter_importer.php"
-  version "1.2.0"
-  sha256 "73de5daccaebb2cd9f5e585f618ec635da392faaa1dda2e192f628a42037ff87"
+  url "https://gist.githubusercontent.com/5ouma/0a5717868f21a29ab96c914ddd55a409/raw/17d5f4dd78b62a4de619854af486c9e4e5b6b6c1/macos_keyboard_shortcuts_exporter_importer.php"
+  version "1.2.1"
+  sha256 "5880aa952d4912c689a0129eb80c1a36e2f1408c528d90db6e20d62f9510003a"
   head "https://gist.github.com/0a5717868f21a29ab96c914ddd55a409.git"
 
   depends_on :macos
