eea8f7da923ff88c62c38dff2baf1e39412cc6ba
Souma <101255979+5ouma@users.noreply.github.com>
2024-02-18 07:44:14 +0900

    chore(gh-dash): Additional OS and arch support
    Add hash for macOS and Linux arm.
    
    Signed-off-by: Souma <101255979+5ouma@users.noreply.github.com>

--- Formula/gh-dash.rb
+++ Formula/gh-dash.rb
@@ -6,15 +6,15 @@ class GhDash < Formula
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/dlvhdr/gh-dash/releases/download/v3.13.1/darwin-amd64"
-      sha256 "abb6d8fcc28f13cdbba4dd0a3ef68b8bc57c4a702b2d393885013a7089094d3a"
+      url "https://github.com/dlvhdr/gh-dash/releases/download/v3.14.0/darwin-amd64"
+      sha256 "7bafc24c408aeb97592ef35ba7d66a012af4ab1a9483ed39abab43bd2a2c22c2"
       def install
         bin.install "darwin-amd64" => name.to_s
       end
     end
     if Hardware::CPU.arm?
-      url "https://github.com/dlvhdr/gh-dash/releases/download/v3.13.1/darwin-arm64"
-      sha256 "0de4c7590faae9e2970355c279cdfc73cab686eb175e127431f0f3307b896a12"
+      url "https://github.com/dlvhdr/gh-dash/releases/download/v3.14.0/darwin-arm64"
+      sha256 "7956868d91409129bc8f3e7df846e9db2195f8991ce0f060559c4f9cd805a6ce"
       def install
         bin.install "darwin-arm64" => name.to_s
       end
@@ -30,8 +30,8 @@ class GhDash < Formula
       end
     end
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/dlvhdr/gh-dash/releases/download/v3.13.1/linux-arm64"
-      sha256 "3f651737c3e85c3983ea30729a245246ada9729fa7aa1820623d184ed2bbd6dc"
+      url "https://github.com/dlvhdr/gh-dash/releases/download/v3.14.0/linux-arm64"
+      sha256 "6a7b8325ff4b253ad890b4cdd582abe524963c016cbcf6fd4129a36e101a1135"
       def install
         bin.install "linux-arm64" => name.to_s
       end
