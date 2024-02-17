9960af8e37881bd1c7cc46c50a29d319b0d3bc87
Souma <101255979+5ouma@users.noreply.github.com>
2024-02-17 10:06:02 +0900

    gh-dash 3.13.1 (#8)

--- Formula/gh-dash.rb
+++ Formula/gh-dash.rb
@@ -1,20 +1,20 @@
 class GhDash < Formula
   desc "Beautiful CLI dashboard for GitHub"
   homepage "https://dlvhdr.github.io/gh-dash"
-  version "3.13.0"
+  version "3.13.1"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/dlvhdr/gh-dash/releases/download/v3.13.0/darwin-amd64"
-      sha256 "22f9bd379f84f4d73f720349e210f57368eed7ef79296b75993e23ecde773d51"
+      url "https://github.com/dlvhdr/gh-dash/releases/download/v3.13.1/darwin-amd64"
+      sha256 "abb6d8fcc28f13cdbba4dd0a3ef68b8bc57c4a702b2d393885013a7089094d3a"
       def install
         bin.install "darwin-amd64" => name.to_s
       end
     end
     if Hardware::CPU.arm?
-      url "https://github.com/dlvhdr/gh-dash/releases/download/v3.13.0/darwin-arm64"
-      sha256 "7546c68f56844ec820b559f1fbf065035c429d48f843cc9eaf429a268bf80229"
+      url "https://github.com/dlvhdr/gh-dash/releases/download/v3.13.1/darwin-arm64"
+      sha256 "0de4c7590faae9e2970355c279cdfc73cab686eb175e127431f0f3307b896a12"
       def install
         bin.install "darwin-arm64" => name.to_s
       end
@@ -23,15 +23,15 @@ class GhDash < Formula
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/dlvhdr/gh-dash/releases/download/v3.13.0/linux-amd64"
-      sha256 "cf94a1919811ecdf549442efde365c02ecdf8f395d81d207d377d530d8bd1ade"
+      url "https://github.com/dlvhdr/gh-dash/releases/download/v3.13.1/linux-amd64"
+      sha256 "4d37519a7bc54394cac9102ccd8aa8df5801ea7900471d3ad7e4432bfeb407e1"
       def install
         bin.install "linux-amd64" => name.to_s
       end
     end
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/dlvhdr/gh-dash/releases/download/v3.13.0/linux-arm64"
-      sha256 "77ceeaaf8a3c4d73ce33e1cf98f836deda247ec5c2a392ee01f47ca7a0c58437"
+      url "https://github.com/dlvhdr/gh-dash/releases/download/v3.13.1/linux-arm64"
+      sha256 "3f651737c3e85c3983ea30729a245246ada9729fa7aa1820623d184ed2bbd6dc"
       def install
         bin.install "linux-arm64" => name.to_s
       end
