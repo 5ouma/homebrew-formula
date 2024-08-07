a60ddc2e8fc75db5e172f1a2e9730d87fcd1327a
Souma <101255979+5ouma@users.noreply.github.com>
2024-08-07 14:58:52 +0900

    fix(formula): Update others
    For other OS and arches.

--- Formula/gh-dash.rb
+++ Formula/gh-dash.rb
@@ -6,15 +6,15 @@ class GhDash < Formula
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/dlvhdr/gh-dash/releases/download/v4.5.1/gh-dash_v4.5.1_darwin-amd64"
-      sha256 "cd89f45b097873d24f279f15384abd1ee5f314752c1147f6396415d3f11a24e8"
+      url "https://github.com/dlvhdr/gh-dash/releases/download/v4.5.2/gh-dash_v4.5.2_darwin-amd64"
+      sha256 "5e60e33f214983a7db2d881876a5220c9fc3865bea1494b23732031657814f18"
       def install
         bin.install "gh-dash_v#{version}_darwin-amd64" => name.to_s
       end
     end
     if Hardware::CPU.arm?
-      url "https://github.com/dlvhdr/gh-dash/releases/download/v4.5.1/gh-dash_v4.5.1_darwin-arm64"
-      sha256 "57d27611e715dea3546afed91a894aa1bbd1b65575d077563031f65bae8a5f12"
+      url "https://github.com/dlvhdr/gh-dash/releases/download/v4.5.2/gh-dash_v4.5.2_darwin-arm64"
+      sha256 "6fc242191b3cc5645fda8ba7811304758cb66fd98e63568f8385be2181293ab0"
       def install
         bin.install "gh-dash_v#{version}_darwin-arm64" => name.to_s
       end
@@ -30,8 +30,8 @@ class GhDash < Formula
       end
     end
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/dlvhdr/gh-dash/releases/download/v4.5.1/gh-dash_v4.5.1_linux-arm64"
-      sha256 "f7c31e5bc0eac77f445dda624733461c4295336ab0aa00755903acf02e6c7aeb"
+      url "https://github.com/dlvhdr/gh-dash/releases/download/v4.5.2/gh-dash_v4.5.2_linux-arm64"
+      sha256 "127d720ae6f589615e1f751a680c318345810a9ba5293f347d1cbd5d9f8be78c"
       def install
         bin.install "gh-dash_v#{version}_linux-arm64" => name.to_s
       end
