1e04974ed58e1222856fa06929e1af423186b639
Souma <101255979+5ouma@users.noreply.github.com>
2024-12-08 18:39:07 +0900

    fix(formula): Revert to the previous `if`
    `on_` not supports `url`.
    
    Signed-off-by: Souma <101255979+5ouma@users.noreply.github.com>

--- Formula/mli.rb
+++ Formula/mli.rb
@@ -9,7 +9,7 @@ class Mli < Formula
   license "MIT"
   depends_on :macos
 
-  on_intel do
+  if Hardware::CPU.intel?
     url "https://github.com/5ouma/mli/releases/download/v0.4.2/mli_Darwin_x86_64.tar.gz"
     sha256 "6f20643b5daf90d791b23e021b3fc41035920a2839bf37b1bf0e6f4dae383f63"
 
@@ -18,7 +18,7 @@ class Mli < Formula
       generate_completions_from_executable("#{bin}/#{name}", "completion")
     end
   end
-  on_arm do
+  if Hardware::CPU.arm?
     url "https://github.com/5ouma/mli/releases/download/v0.4.2/mli_Darwin_arm64.tar.gz"
     sha256 "b10089aa86a9e9e0c136f1c092f41cda6c10c03e4c0a8133268579055dfa09b7"
 
