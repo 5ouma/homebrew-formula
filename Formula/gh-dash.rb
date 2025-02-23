42946e2a7de26e549ae07315aa1cd1a9e9edad5c
Souma <101255979+5ouma@users.noreply.github.com>
2025-02-23 00:53:53 +0000

    gh-dash 4.12.0

--- Formula/gh-dash.rb
+++ Formula/gh-dash.rb
@@ -1,7 +1,7 @@
 class GhDash < Formula
   desc "Beautiful CLI dashboard for GitHub"
   homepage "https://dlvhdr.github.io/gh-dash"
-  version "4.11.0"
+  version "4.12.0"
   license "MIT"
   depends_on "gh"
 
@@ -24,8 +24,8 @@ class GhDash < Formula
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/dlvhdr/gh-dash/releases/download/v4.11.0/gh-dash_v4.11.0_linux-amd64"
-      sha256 "53f6387b5e72bc7fb029585dce35166fe580de107c26046cd038fd71dc86d18a"
+      url "https://github.com/dlvhdr/gh-dash/releases/download/v4.12.0/gh-dash_v4.12.0_linux-amd64"
+      sha256 "a68697fa937dd2178c29e780c9035387b88318c372218b937e7b4d2d4b4cf100"
       def install
         bin.install "gh-dash_v#{version}_linux-amd64" => name.to_s
       end
